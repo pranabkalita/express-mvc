eb3da653257a807473767738f0f7b7235640a082
Pranab Kalita <pavankalita@gmail.com>
2022-03-08 18:50:20 +0530

    Initial Commit - Setup Express Server

---
+++ server.js
@@ -0,0 +1,23 @@
+// Global Imports
+import express from 'express'
+
+// Constants
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost'
+
+// Initialize App
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('Welcome !')
+})
+
+// Start Server
+app.listen(port, host, () => {
+  console.info(`Server started on port http://${host}:${port}`)
+})
